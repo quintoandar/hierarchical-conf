1aeec6cad30a693bd9f8051e242a78e559fff5bf
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2022-08-15 11:53:28 -0300

    Refactor: remove unecessary instance variable

--- tests/integration/test_hierarchical_conf.py
+++ tests/integration/test_hierarchical_conf.py
@@ -5,39 +5,39 @@ from unittest import mock
 from hierarchical_conf.hierarchical_conf import HierarchicalConf
 
 
-@mock.patch.dict(os.environ, {"ENVIRONMENT": "integration_env"})
-def test_load_confs():
-    # arrange
-    expected_confs = {
-        "key1": "value of key1...",
-        "key2": "value of key2...",
-        "key3": "value of key3...",
-    }
-    integration_tests_folder = dirname(__file__)
+class TestHierarchicalConf:
+    @mock.patch.dict(os.environ, {"ENVIRONMENT": "integration_env"})
+    def test_load_confs(self):
+        # arrange
+        expected_confs = {
+            "key1": "value of key1...",
+            "key2": "value of key2...",
+            "key3": "value of key3...",
+        }
+        integration_tests_folder = dirname(__file__)
 
-    # act
-    hierarchical_conf = HierarchicalConf(searched_paths=[integration_tests_folder])
+        # act
+        hierarchical_conf = HierarchicalConf(searched_paths=[integration_tests_folder])
 
-    # assert
-    assert hierarchical_conf.configs == expected_confs
+        # assert
+        assert hierarchical_conf.configs == expected_confs
 
+    @mock.patch.dict(os.environ, {"ENVIRONMENT": "integration_env"})
+    def test_load_confs(self):
+        # arrange
+        expected_confs = {
+            "key1": "value of key1...",
+            "key2": "This is another value from "
+            "precedence_example/integration_env_conf.yml...",
+            "key3": {"foo": "new value of key3.foo"},
+        }
+        integration_tests_folder = dirname(__file__)
+        precedence_case_folder = dirname(__file__) + "/precedence_case/"
 
-@mock.patch.dict(os.environ, {"ENVIRONMENT": "integration_env"})
-def test_load_confs():
-    # arrange
-    expected_confs = {
-        "key1": "value of key1...",
-        "key2": "This is another value from "
-        "precedence_example/integration_env_conf.yml...",
-        "key3": {"foo": "new value of key3.foo"},
-    }
-    integration_tests_folder = dirname(__file__)
-    precedence_case_folder = dirname(__file__) + "/precedence_case/"
+        # act
+        hierarchical_conf = HierarchicalConf(
+            searched_paths=[integration_tests_folder, precedence_case_folder]
+        )
 
-    # act
-    hierarchical_conf = HierarchicalConf(
-        searched_paths=[integration_tests_folder, precedence_case_folder]
-    )
-
-    # assert
-    assert hierarchical_conf.configs == expected_confs
+        # assert
+        assert hierarchical_conf.configs == expected_confs
