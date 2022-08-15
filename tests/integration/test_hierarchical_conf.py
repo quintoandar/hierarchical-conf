8ba0e49d68746ba76d16a3efa74a376f4730d51a
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2022-08-15 11:31:32 -0300

    [DIN-30] Adding integration tests (#5)

---
+++ tests/integration/test_hierarchical_conf.py
@@ -0,0 +1,43 @@
+import os
+from os.path import dirname
+from unittest import mock
+
+from quintoandar_hierarchical_conf.hierarchical_conf import HierarchicalConf
+
+
+@mock.patch.dict(os.environ, {"ENVIRONMENT": "integration_env"})
+def test_load_confs():
+    # arrange
+    expected_confs = {
+        "key1": "value of key1...",
+        "key2": "value of key2...",
+        "key3": "value of key3...",
+    }
+    integration_tests_folder = dirname(__file__)
+
+    # act
+    hierarchical_conf = HierarchicalConf(searched_paths=[integration_tests_folder])
+
+    # assert
+    assert hierarchical_conf.configs == expected_confs
+
+
+@mock.patch.dict(os.environ, {"ENVIRONMENT": "integration_env"})
+def test_load_confs():
+    # arrange
+    expected_confs = {
+        "key1": "value of key1...",
+        "key2": "This is another value from "
+        "precedence_example/integration_env_conf.yml...",
+        "key3": {"foo": "new value of key3.foo"},
+    }
+    integration_tests_folder = dirname(__file__)
+    precedence_case_folder = dirname(__file__) + "/precedence_case/"
+
+    # act
+    hierarchical_conf = HierarchicalConf(
+        searched_paths=[integration_tests_folder, precedence_case_folder]
+    )
+
+    # assert
+    assert hierarchical_conf.configs == expected_confs
