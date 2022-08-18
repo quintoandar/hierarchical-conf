10718237c27557e28cb53954257f766ed7f19ba5
Lucas Fonseca <lucas.fonseca@quintoandar.com.br>
2022-08-18 11:13:04 -0300

    Load only existent files (#20)

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -6,7 +6,7 @@ from hierarchical_conf.hierarchical_conf import HierarchicalConf
 
 
 @pytest.fixture()
-@mock.patch.object(HierarchicalConf, "_validate_if_config_file_exists")
+@mock.patch.object(HierarchicalConf, "_config_file_exists")
 @mock.patch.object(HierarchicalConf, "_read_configuration")
-def hierarchical_conf(mock_read_configuration, mock_validate_if_config_file_exists):
+def hierarchical_conf(mock_read_configuration, mock_config_file_exists):
     return HierarchicalConf(searched_paths=[mock.ANY])
